1509ae297d61fbf83b23c54b6b6e46e206e35053
erandros <guidotarsia@gmail.com>
2017-03-06 12:03:44 -0300

    Initial commit

---
+++ spec/atom-yetiman-core-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomYetimanCoreView from '../lib/atom-yetiman-core-view';
+
+describe('AtomYetimanCoreView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
